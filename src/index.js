857c3571f9782e24208ce3da3f8f0a51ae3fff59
Branko Durdevic <branko@minka.io>
2023-02-14 10:27:07 +0100

    Abort Debit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { logRequest } from './middleware/logging.js'
 import { asyncErrorWrapper, handleErrors } from './middleware/errors.js'
 import { commitCredit, prepareCredit } from './handlers/credits.js'
-import { commitDebit, prepareDebit } from './handlers/debits.js'
+import { abortDebit, commitDebit, prepareDebit } from './handlers/debits.js'
 
 const bankName = 'Demo bank'
 const port = 3001
@@ -22,6 +22,7 @@ app.post('/credits/:handle/commit', asyncErrorWrapper(commitCredit))
 
 app.post('/debits', asyncErrorWrapper(prepareDebit))
 app.post('/debits/:handle/commit', asyncErrorWrapper(commitDebit))
+app.post('/debits/:handle/abort', asyncErrorWrapper(abortDebit))
 
 app.use(handleErrors)
 
