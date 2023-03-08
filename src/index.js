2cfb63ba29c0202e9288b3003b3e0eb60f0d0a19
Branko Durdevic <branko@minka.io>
2023-03-08 14:56:49 +0100

    Logging requests and errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import { logRequest } from './middleware/logging.js'
+import { handleErrors } from './middleware/errors.js'
 
-const app = express()
 const bankName = 'Demo bank'
 const port = 3001
+
+const app = express()
+
+app.use(express.json())
+
+app.use(logRequest)
 
 app.get('/', (req, res) => {
   res.send(`${bankName} is running!`)
 })
+
+app.use(handleErrors)
 
 app.listen(port, () => {
   console.log(`${bankName} running on port ${port}`)
---
+++ src/middleware/errors.js
@@ -0,0 +1,19 @@
+// Necessary to properly handle async errors
+export function asyncErrorWrapper(func) {
+  return async (req, res, next) => {
+    try {
+      return await func(req, res, next)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+// This needs to go after all route handlers to log the errors
+// and send the appropriate response to the client.
+export function handleErrors(err, req, res, next) {
+  console.log(err)
+  if (!res.headersSent) {
+    res.sendStatus(500)
+  }
+}
